2a0cd80ad3d464235d3d39f4634b95ac90fda218
Ritik <rgaur5713@conestogac.on.ca>
2024-08-13 12:56:53 -0400

    create routing and basic pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,38 @@
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "/sign-up",
+      element: <SignUp />,
+    },
+    {
+      path: "/projects",
+      element: <Projects />,
+    },
+  ]);
 
   return (
     <>
-      <div className="text-4xl">App</div>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
